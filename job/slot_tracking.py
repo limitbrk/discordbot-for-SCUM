aeb0ca620caf83340e9f70c7f9da1e9cded0d8fe
Chaiyatat Meema <33203612+limitbrk@users.noreply.github.com>
2022-09-08 18:03:35 +0000

    Fix Status handle

--- job/slot_tracking.py
+++ job/slot_tracking.py
@@ -9,14 +9,17 @@ async def run(logging: logging, bot: discord.Client,server_id: str) :
 
     info_response = requests.get(server_url)
     if info_response.status_code == 200:
-        info = info_response.json()['data']['attributes']
-        status  = "🟢" if info['status'] == 'online' else  "🔴"
-        online  = info['players']
-        max     = info['maxPlayers']
+      info = info_response.json()['data']['attributes']
+      status_flag   = discord.Status.online if info['status'] == 'online' else  discord.Status.dnd
+      online        = info['players']
+      max           = info['maxPlayers']
 
-        status_name = "🙋{0}/{1} {2}".format(online,max,status)
-        await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=status_name))
-        logging.info("data updated! : {0}".format(status_name))
-
+      status_name = "🙋{0}/{1}".format(online,max)
+      await bot.change_presence(status=status_flag, activity=discord.Activity(type=discord.ActivityType.playing, name=status_name))
+      logging.info(
+        "data updated! : {0} \"{1}\""
+        .format(str(status_flag)
+        .replace(str(discord.Status.dnd),"offline")
+        ,status_name))
 def encode_time(datetime: datetime.datetime) : 
     return urllib.parse.quote(datetime.strftime("%Y-%m-%dT%H:%M:%S.000Z"))
