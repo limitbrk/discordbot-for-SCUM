f4953c60ff34d76793b758ac0479745b77f3537a
Chaiyatat Meema <33203612+limitbrk@users.noreply.github.com>
2022-09-08 12:18:41 +0000

    add repl.it support

--- job/slot_tracking.py
+++ job/slot_tracking.py
@@ -2,9 +2,9 @@ import discord
 import datetime
 import requests
 import urllib.parse
-import flag
+import logging
 
-async def run(bot: discord.Client,server_id: str) :
+async def run(logging: logging, bot: discord.Client,server_id: str) :
     server_url = "https://api.battlemetrics.com/servers/{0}".format(server_id)
 
     info_response = requests.get(server_url)
@@ -14,7 +14,9 @@ async def run(bot: discord.Client,server_id: str) :
         online  = info['players']
         max     = info['maxPlayers']
 
-        await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="🙋{0}/{1} {2}".format(online,max,status)))
+        status_name = "🙋{0}/{1} {2}".format(online,max,status)
+        await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=status_name))
+        logging.info("data updated! : {0}".format(status_name))
 
 def encode_time(datetime: datetime.datetime) : 
     return urllib.parse.quote(datetime.strftime("%Y-%m-%dT%H:%M:%S.000Z"))
--- main.py
+++ main.py
@@ -3,28 +3,42 @@ import discord
 from discord.ext import tasks
 from dotenv import load_dotenv
 from job import slot_tracking
+from server import route
+import logging
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 WELCOME_ID = os.getenv('WELCOME_CHANNEL_ID')
 SERVER_ID = os.getenv('BATTLEMETRICS_SERVER_ID')
 
+logging.basicConfig(
+  format='%(asctime)s %(levelname)-8s %(message)s',
+  level=logging.INFO,
+  datefmt='%Y-%m-%d %H:%M:%S')
+
 intents = discord.Intents.default()
 intents.message_content = True
 bot = discord.Client(intents=intents)
-if WELCOME_ID != '' :
+if WELCOME_ID != '':
     welcome = bot.get_channel(636399538650742795)
+
 
 @bot.event
 async def on_ready():
-    print(f'{bot.user} has connected to Discord!')
+    logging.debug(f'{bot.user} has connected to Discord!')
+    logging.debug(f'Now tracked BattleMetrics ID "{SERVER_ID}"')
     update_slot.start()
-async def on_member_join(member):
-    if welcome :
-        await welcome.send(f" Welcome {member.mention} To HAPPY LAND PvE 100% No Mech [LOOT x2.5] 🎉\n🚩 กฎ-กติกาเซิร์ฟ กฎ-กติกาเซิร์ฟ\n📝 ลงทะเบียน #ลงทะเบียน")
+
+
+# TBD
+# async def on_member_join(member):
+#     if welcome :
+#         await welcome.send(f" Welcome {member.mention} To HAPPY LAND PvE 100% No Mech [LOOT x2.5] 🎉\n🚩 กฎ-กติกาเซิร์ฟ กฎ-กติกาเซิร์ฟ\n📝 ลงทะเบียน #ลงทะเบียน")
+
 
 @tasks.loop(minutes=1)
 async def update_slot():
-    await slot_tracking.run(bot,SERVER_ID)
+    await slot_tracking.run(logging, bot, SERVER_ID)
 
+route.keep_alive()
 bot.run(TOKEN)
