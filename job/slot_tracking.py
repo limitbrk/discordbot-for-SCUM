cad24e562e7af4925621b794408b1bae74f42347
Chaiyatat Meema <33203612+limitbrk@users.noreply.github.com>
2022-09-09 02:53:41 +0000

    add status idle and edit emoji

--- job/slot_tracking.py
+++ job/slot_tracking.py
@@ -10,16 +10,23 @@ async def run(logging: logging, bot: discord.Client,server_id: str) :
     info_response = requests.get(server_url)
     if info_response.status_code == 200:
       info = info_response.json()['data']['attributes']
-      status_flag   = discord.Status.online if info['status'] == 'online' else  discord.Status.dnd
+      
       online        = info['players']
       max           = info['maxPlayers']
+      
+      status_flag   = discord.Status.dnd
+      if online < 1 :
+        status_flag = discord.Status.idle
+      elif info['status'] == 'online' :
+        status_flag = discord.Status.online
 
-      status_name = "🙋{0}/{1}".format(online,max)
+      status_name = "🏃{0}/{1}".format(online,max)
       await bot.change_presence(status=status_flag, activity=discord.Activity(type=discord.ActivityType.playing, name=status_name))
       logging.info(
         "data updated! : {0} \"{1}\""
         .format(str(status_flag)
         .replace(str(discord.Status.dnd),"offline")
+        .replace(str(discord.Status.idle),"sleep  ")
         ,status_name))
 def encode_time(datetime: datetime.datetime) : 
     return urllib.parse.quote(datetime.strftime("%Y-%m-%dT%H:%M:%S.000Z"))
