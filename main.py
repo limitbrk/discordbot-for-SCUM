a56d368e9853292dd8c9f2f01578e5b20d181d9a
Chaiyatat Meema <limitbrk41@gmail.com>
2022-09-30 09:50:55 +0700

    add ftp support

--- main.py
+++ main.py
@@ -9,19 +9,15 @@ import logging
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 SERVER_ID = os.getenv('BATTLEMETRICS_SERVER_ID')
-FTP_CONFIG = {
-  "host": os.getenv('FTP_HOST'),
-  "port": os.getenv('FTP_PORT'),
-  "username": os.getenv('FTP_USERNAME'),
-  "password": os.getenv('FTP_PASSWORD'),
-  "logspath": os.getenv('FTP_LOGS_FOLDER_PATH'),
-  "channel": {
-    "kill":    os.getenv('CHANNEL_ID_KILL_LOGS'),
-    "mines":   os.getenv('CHANNEL_ID_MINES_LOGS'),
-    "economy": os.getenv('CHANNEL_ID_ECOMONY_LOGS'),
-    "chat":    os.getenv('CHANNEL_ID_CHAT_LOGS')
-  }
-}
+FTP_CONFIG_HOST = os.getenv('FTP_HOST')
+FTP_CONFIG_PORT = int(os.getenv('FTP_PORT'))
+FTP_CONFIG_USERNAME = os.getenv('FTP_USERNAME')
+FTP_CONFIG_PASSWORD = os.getenv('FTP_PASSWORD')
+FTP_CONFIG_LOGSPATH = os.getenv('FTP_LOGS_FOLDER_PATH')
+CH_KILL =os.getenv('CHANNEL_ID_KILL_LOGS'),
+CH_MINES=os.getenv('CHANNEL_ID_MINES_LOGS'),
+CH_ECO  =os.getenv('CHANNEL_ID_ECOMONY_LOGS'),
+CH_CHAT =os.getenv('CHANNEL_ID_CHAT_LOGS')
 
 logging.basicConfig(
   format='%(asctime)s %(levelname)-8s %(message)s',
@@ -31,14 +27,20 @@ logging.basicConfig(
 intents = discord.Intents.default()
 intents.message_content = True
 bot = discord.Client(intents=intents)
-log = log_notifier.LogNotifier(bot, FTP_CONFIG)
+log = log_notifier.LogJob(
+  bot, 
+  FTP_CONFIG_HOST,
+  FTP_CONFIG_PORT,
+  FTP_CONFIG_USERNAME,
+  FTP_CONFIG_PASSWORD
+)
 
 @bot.event
 async def on_ready():
   logging.debug(f'{bot.user} has connected to Discord!')
   logging.debug(f'Now tracked BattleMetrics ID "{SERVER_ID}"')
   update_slot.start()
-  # LogNotifier.run()
+  log.add_job(1018112696580845568,FTP_CONFIG_LOGSPATH,"kill")
 
 @tasks.loop(minutes=1)
 async def update_slot():
