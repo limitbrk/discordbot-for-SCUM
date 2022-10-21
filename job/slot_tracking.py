a7ab9a4e2aabbc1fe919bfa5a79832682634ec04
Chaiyatat Meema <33203612+limitbrk@users.noreply.github.com>
2022-10-21 08:32:03 +0000

    update from repl

--- job/slot_tracking.py
+++ job/slot_tracking.py
@@ -2,8 +2,11 @@ import discord
 import requests
 import logging
 
+clock_emoji   = '🕛🕐🕑🕒🕓🕔🕕🕖🕗🕘🕙🕚'
+weather_emoji = '☀️🌙'
+
 async def run(bot: discord.Client,server_id: str) :
-    server_url = "https://api.battlemetrics.com/servers/{0}".format(server_id)
+    server_url = 'https://api.battlemetrics.com/servers/{0}'.format(server_id)
 
     info_response = requests.get(server_url)
     if info_response.status_code == 200:
@@ -11,14 +14,23 @@ async def run(bot: discord.Client,server_id: str) :
       
       online        = info['players']
       max           = info['maxPlayers']
-      
+
       status_flag   = discord.Status.dnd
       if online < 1 :
         status_flag = discord.Status.idle
       elif info['status'] == 'online' :
         status_flag = discord.Status.online
 
-      status_name = "🏃{0}/{1}".format(online,max)
+      
+      hour          = int(info['details']['time'].split(':')[0])
+      status_time   = f"{clock_emoji[hour%12]}{hour}"
+      
+      status_weather = '🌙'
+      if 5 < hour < 23 :
+        status_weather = '☀️'
+      
+      status_name = f"🏃{online}/{max} {status_time}{status_weather}"
+      print(status_name)
       await bot.change_presence(status=status_flag, activity=discord.Activity(type=discord.ActivityType.playing, name=status_name))
       logging.info(
         "data updated! : {0} \"{1}\""
